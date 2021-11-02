6f6aa3781f466b7ad4a57d6b07ccd87f93ca0f48
wakey001 <stefanwakeman001@hotmail.co.uk>
2021-11-02 00:53:30 +0000

    add some style

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+document.addEventListener('DOMContentLoaded', () => {
+const userGrid = document.querySelector('grid-user')
+const computerGrid = document.querySelector('grid-computer')
+const displayGrid = document.querySelector('grid-display')
+const ships = document.querySelectorAll('ship')
+
+})
+
+
+
